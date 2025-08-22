b35f8ab465180202c5c2e35c17640dfde73f04df
Zeeshan Munir <zeeshan.x01000@gmail.com>
2025-08-22 15:56:57 +0500

    25-08-22_15:56

---
+++ 25-08-22/01.py
@@ -0,0 +1,8 @@
+name: str = "Zeeshan"
+age: int = 22
+cgpa: float = 3.1
+is_student: bool = True
+skills: list[str] = ["Python", "Nextjs", "Tailwindcss"]
+tuple_example: tuple[int, str, float] = (1, "Zeeshan", 3.1)
+print(f"Name: {name}\nAge: {age}\nCGPA: {cgpa}\nStudent: {is_student}\nSkills: {skills} \nTuple: {tuple_example}")
+
---
+++ 25-08-22/02.py
@@ -0,0 +1,4 @@
+# LISTs:
+list1 = [1, 2.3, "three", False, 5]
+print(list1)
+print(list1[1:4])
